7b63369f9e5af4cefe1beb070a3a347743a1aadb
Rijosh <rijosh.ravi@phases.io>
2023-09-07 23:11:34 +0530

    error message styles

--- frontend/src/components/WeatherForecast.vue
+++ frontend/src/components/WeatherForecast.vue
@@ -2,15 +2,15 @@
   <div class="hello">
     <h3 class="text-2xl font-semibold mb-4">Weather forecast for city</h3>
 
-    <div v-if="loading">
-      <p>Loading...</p>
+    <div v-if="loading" class="text-center">
+      <p class="text-gray-600">Loading weather data...</p>
     </div>
-    <div v-else-if="error">
-      <p>Oops! Something went wrong. Please try again.</p>
+    <div v-else-if="error" class="text-center text-red-600 font-semibold">
+      <p>Oops! Something went wrong. Please try again later.</p>
     </div>
     <div
       v-else-if="weatherData"
-      class="grid grid-cols-2 gap-4 justify-center items-center px-40"
+      class="grid grid-cols-2 gap-4 justify-center items-center px-40 pb-10"
     >
       <div class="relative bg-blue-200 p-4 rounded-lg">
         <p class="text-gray-600">Temperature</p>
