309a3cc99c34bd716652c290e7805833cfba4269
Rijosh <rijosh.ravi@phases.io>
2023-09-07 23:05:12 +0530

    styled view for weather data

--- frontend/src/components/WeatherForecast.vue
+++ frontend/src/components/WeatherForecast.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h3>Weather forecast for city</h3>
+    <h3 class="text-2xl font-semibold mb-4">Weather forecast for city</h3>
 
     <div v-if="loading">
       <p>Loading...</p>
@@ -8,19 +8,25 @@
     <div v-else-if="error">
       <p>Oops! Something went wrong. Please try again.</p>
     </div>
-    <div v-else-if="weatherData">
-      <div>
-        <p>Temperature: {{ weatherData?.temperature }}</p>
+    <div
+      v-else-if="weatherData"
+      class="grid grid-cols-2 gap-4 justify-center items-center px-40"
+    >
+      <div class="relative bg-blue-200 p-4 rounded-lg">
+        <p class="text-gray-600">Temperature</p>
+        <p class="text-3xl font-semibold">
+          {{ weatherData?.temperature }} &deg;C
+        </p>
       </div>
-      <div>
-        <p>Wind Direction: {{ weatherData?.winddirection }}</p>
+      <div class="relative bg-blue-200 p-4 rounded-lg">
+        <p class="text-gray-600">Wind Speed</p>
+        <p class="text-3xl font-semibold">{{ weatherData?.windspeed }} km/h</p>
       </div>
-      <div>
-        <p>Wind Speed: {{ weatherData?.windspeed }}</p>
+      <div class="relative bg-blue-200 p-4 rounded-lg">
+        <p class="text-gray-600">Wind Direction</p>
+        <p class="text-3xl font-semibold">{{ weatherData?.winddirection }}</p>
       </div>
     </div>
-
-    <div></div>
   </div>
 </template>
 
